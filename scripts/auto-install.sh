edc9384db5b9ab7413cd85d0fdaa9ecced5f00c1
xgqfrms <xgqfrms@outlook.com>
2021-10-09 10:57:59 +0800

    Create auto-install.sh

---
+++ scripts/auto-install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# sh/zsh/bash 选择默认
+
+# echo "🚀 linux shell 定时器" && npm i -g app-node-env
+echo "🚀 linux shell 定时器" && yarn global add app-node-env
+
+# Linux Bash shell 脚本定时器 All In One
+# https://www.cnblogs.com/xgqfrms/p/15384401.html
